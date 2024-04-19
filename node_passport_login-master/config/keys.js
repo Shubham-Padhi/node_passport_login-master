b760bad89d04dc1229c1a45f8452aad9f2ff0bcd
Shubham Padhi <shubhampadhi404@gmail.com>
2024-04-20 01:29:49 +0530

    first commit

---
+++ node_passport_login-master/config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://shubhampadhi404:'+ encodeURIComponent(rywtEgEkesusXKYS) + '@cluster0.l92ox7f.mongodb.net/jobPortal?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
